0876c9726ef83e4843da75b70e3d13706bea77b1
Arushi2798 <arushi1301@gmail.com>
2025-04-12 11:52:25 +0530

    code update

--- prime.cpp
+++ prime.cpp
@@ -2,6 +2,30 @@
 using namespace std;
 
 int main(){
+    int n;
+    cout << "enter a number you wanna cheack :";
+    cin >> n;
+    bool isprime=true;
+
+    for(int i=2 ; i*i <=n ; i++){
+        if (n % i == 0){
+            isprime=false;
+            break;
+        }
+    }
+
+    // for(int i=2 ; i <= n-1 ; i++){
+    //     if (n % i == 0){
+    //         isprime=false;
+    //         break;
+    //     }
+    // }
+
+    if (isprime==true){
+        cout<<"it's a prime number\n";
+    }else{
+        cout<<"it's not prime\n";
+    }
 
     return 0;
 }
