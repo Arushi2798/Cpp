59e5612ad39b90ab92e433fb4e4104402e357886
Arushi2798 <arushi1301@gmail.com>
2025-04-12 11:27:01 +0530

    code update

--- practise.cpp
+++ practise.cpp
@@ -2,10 +2,47 @@
 using namespace std;
 
 int main(){
-    int a,b;
-    cin >> a;
-    cin >> b; 
-    cout<< a+b << endl;
+    // //basic practise
+    for (int n=4;n<=10;n++){
+        cout<<n<<" ";
+    }
+    
+    // int n=0;
+    // while (n < 5){
+    //     cout<<n;
+    //     n++;
+        
+    // }
+    cout<<endl;
+
+    // char a='a';
+    // cout<< (a>=65 && a <=90 ?"upper":"lower")<<endl;
+
+    // int a=5;
+    // cout<<(a>2 ? "good":"bad")<<endl;
+
+    // int a=3;
+    // int b=++a;
+    // cout<<b<<"\n"<<a<<endl;
+
+    // float a=49.5; 
+    // int b=7;
+    // cout<<((int)a/b)<<endl;
+
+    // int age;
+    // cout<< "enter your age: ";
+    // cin>> age;
+    // cout<< "your age is: "<<age;
+
+    // char grade = 'A';
+    // float price= 99.99;
+    // double annualSalary = 1800000.0000;
+    // bool happy = true;
+    // cout << "your age: " << age<< "\n your salary: "<<annualSalary<<"\nyou happy?: "<<happy<<"\nyour grade?: "<< grade<<"\n\nprice of this item is "<<price<<endl; 
+    
+    // int newprice= price;
+    // cout<< newprice<<"\n";
+    
     system("pause"); 
     return 0;
 }
