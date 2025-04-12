a0873a3749ce6f46aa05066320bc6dae5803328b
Arushi2798 <arushi1301@gmail.com>
2025-04-12 14:28:32 +0530

    code update

--- patterns.cpp
+++ patterns.cpp
@@ -2,28 +2,192 @@
 using namespace std;
 
 int main(){
-    
-    
-    // to print 
-    // 1 2 3 
-    // 4 5 6
-    // 7 8 9
-    // 10 11 12
-    
-    int x,n;
 
-    cout<<"number of rows: ";
+    // to print hollow diamond pattern
+    //       *
+    //     *    *
+    //   *        *
+    // *            *
+    //   *        *
+    //     *    *
+    //       *
+    
+    int x;
+    cout<<"size of daimond: ";
     cin>>x;
-    cout<<"numbers from: ";
-    cin>>n;
-
-    for (int i=0; i<x ;i++){        
-        for (int j= 0 ; j< 3; j++){
-            cout<<n<<" ";
-            n++;
+    
+    for (int i=0 ; i<x ; i++){
+        for (int k=x-i-1;k>0;k--){
+            cout<<"  ";
         }
+        cout<<"*";
+        for (int j=0; j<i ;j++){
+            cout<<"    ";
+        }
+        if (i>0){         
+        cout<<"*";}
         cout<<endl;
     }
+
+    for (int i=1 ; i<x ; i++){
+        for (int k=0 ; k<i ; k++){
+            cout<<"  ";
+        }
+        cout<<"*";
+
+        for (int j=x-i-1; j>0 ;j--){
+            cout<<"    ";
+        }
+        if (i<x-1){         
+        cout<<"*";}
+        cout<<endl;
+
+    }
+
+    // // to print pyramid pattern
+    // //    1
+    // //   121
+    // //  12321
+    // // 1234321
+    
+    // int x;
+    // cout<<"rows number: ";
+    // cin>>x;
+    
+    // for (int i=0 ; i<x ; i++){
+    //     for (int k=x-i-1 ; k>0 ; k--){
+    //         cout<<" ";
+    //     }
+    //     for (int j=0; j < i+1 ;j++){
+    //         cout<<j+1;
+    //     }
+    //     for (int l= i ; l>0; l--){
+    //                 cout<<l;     
+    //     }
+    //     cout<<endl;
+    // }
+
+    // // to print 
+    // // 1111
+    // //  222
+    // //   33
+    // //    4
+    
+    // int x;
+    // cout<<"rows number: ";
+    // cin>>x;
+    
+    // for (int i=0 ; i<x ; i++){
+    //     for (int k=0; k<i;k++){
+    //         cout<<" ";
+    //     }
+    //     for (int j=0; j < x-i ;j++){
+    //         cout<<i+1;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // // to print floyd's triangle
+    // // 1
+    // // 23
+    // // 456
+    
+    // int x,num;
+    // cout<<"rows number: ";
+    // cin>>x;
+    // cout<<"numbers from: ";
+    // cin>>num;
+
+    // for (int i=0 ; i<x ; i++){
+    //     for (int j=0 ; j<=i; j++){
+    //         cout<<num;
+    //         num++;
+    //     }
+    //     cout<<endl;
+    // }
+
+    // // to print 
+    // // 1
+    // // 21
+    // // 321
+    // // 4321
+    
+    // int x;
+
+    // cout<<"highest number: ";
+    // cin>>x;
+    
+    // for (int i=0 ; i<x ; i++){
+    //     for (int j= i+1 ; j>0; j--){
+    //         cout<<j;         
+    //     }
+    //     cout<<endl;
+    // }
+
+    // // to print 
+    // // 4
+    // // 43
+    // // 432
+    // // 4321
+    
+    // int x;
+
+    // cout<<"highest number: ";
+    // cin>>x;
+    
+    // for (int i=0 ; i<x ; i++){
+    //     int num=x;
+    //     for (int j= 0 ; j<= i; j++){
+    //         cout<<num;
+    //         num--;           
+    //     }
+    //     cout<<endl;
+    // }
+
+    // // to print 
+    // // 10 
+    // // 11 11
+    // // 12 12 12
+    // // 13 13 13 13
+    
+    // int x,n;
+
+    // cout<<"number of rows: ";
+    // cin>>x;
+    // cout<<"numbers from: ";
+    // cin>>n;
+
+    // for (int i=0;i<x;i++){
+    //     for (int j= 0 ; j<=i ; j++){
+    //         cout<<n<<" ";
+            
+    //     }
+    //     n++;
+    //     cout<<endl;
+    // }
+    
+    // // to print with x=4,n=1,y=3
+    // // 1 2 3 
+    // // 4 5 6
+    // // 7 8 9
+    // // 10 11 12
+    
+    // int x,n,y;
+
+    // cout<<"number of rows: ";
+    // cin>>x;
+    // cout<<"numbers from: ";
+    // cin>>n;
+    // cout<<"number in each row? : ";
+    // cin>>y;
+
+    // for (int i=0; i<x ;i++){        
+    //     for (int j= 0 ; j< y; j++){
+    //         cout<<n<<" ";
+    //         n++;
+    //     }
+    //     cout<<endl;
+    // }
 
     // // to print 
     // // ABC
