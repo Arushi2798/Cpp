e7d466f51246ee73d6123a95990bf491340487a8
Arushi2798 <arushi1301@gmail.com>
2025-04-16 23:33:49 +0530

    code update

---
+++ binary.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// from decimal to binary
+int main(){
+    int n=5;
+    while (n>0){
+        int r = n%2;
+        int d=n/2;
+        
+    }
+
+    return 0;
+}
